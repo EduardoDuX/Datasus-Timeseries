70ebe7487772cba87917860f516603b630c75e1d
pedro_maconetto <pedro.maconetto@2morrowsports.com.br>
2023-10-10 14:00:58 -0300

    mudança

--- functions/preprocess.py
+++ functions/preprocess.py
@@ -1,7 +1,17 @@
-def preprocess_SINAN(df):
+def preprocess_SINAN(df, agrupamento):
+    if agrupamento == 'Anual':
+        df['DATA'] = df['DATA'].apply(lambda x: x[:4])
+    if agrupamento == 'Mensal':
+        df['DATA'] = df['DATA'].apply(lambda x: x[:7])
     df = df.groupby(['DT_NOTIFIC']).count().reset_index().rename(columns={'DT_NOTIFIC': 'Data','ID_AGRAVO': 'Casos'})
     return df
 
-def preprocess_SIM(df):
-    df = df.groupby(['DATA']).count().reset_index().rename(columns={'DATA': 'Data','Unnamed: 0': 'Casos'})
-    return df
+def preprocess_SIM(df, agrupamento):
+    df2 = df.copy()
+    if agrupamento == 'Anual':
+        df2['DATA'] = df2['DATA'].apply(lambda x: x[:4])
+    if agrupamento == 'Mensal':
+        df2['DATA'] = df2['DATA'].apply(lambda x: x[:7])
+
+    df2 = df2.groupby(['DATA']).count().reset_index().rename(columns={'DATA': 'Data','Unnamed: 0': 'Casos'})
+    return df2
